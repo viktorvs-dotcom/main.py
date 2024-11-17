e2976bee0646791f5249e74bdaf7af004af9b3cc
viktorvs-dotcom <viktorvs1987@gmail.com>
2024-11-17 21:00:51 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# "1st programm"
+print('"1st programm,"')
+print('- арифметика;')
+print(9 ** 0.5 * 5)
+
+# "2nd programm"
+print('"2nd programm"')
+print('- логика;')
+print(9.99 > 9.98 and 1000 != 100.1)
+
+# "3rd program"
+print('"3rd program"')
+
+print('- школьная загадка')
+print(2 * 2 + 2)  # без приоритетов
+print(2 * (2 + 2))  # с приоритетом
+print((2 * 2 + 2) == (2 * (2 + 2)))  # равны ли эти выражения?
+
+# "4th program"
+
+print('"4th program"')
+print('-первый после точки')
+str = '123.456'
+print(str[4])
+flo_t = float(str)
+flo_t = flo_t * 10
+int = flo_t/10
+
+print(flo_t)
+print(int)
